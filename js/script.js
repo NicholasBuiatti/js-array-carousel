d637ec64d56685a6c0441a30254abc14fe7ce50c
NicholasBuiatti <nic.buiatti@gmail.com>
2024-04-23 20:22:08 +0200

    m-3 button next

--- js/script.js
+++ js/script.js
@@ -7,32 +7,32 @@ console.log(Slider);
 let visibleSlide;
 let nextSlide;
 
-
-// conteggio oggetti nell'array
-for (let i = 0; i < Slider.length; i++) {
-    const img = Slider[i];
-    console.log(img);
-    // selezione oggetto con l'attributo che cerco e lo cambio
-    if (img.classList.contains('visible')) {
-        img.classList.remove('visible');
-        console.log(i, img);
-        // salvo l'elemento
-        visibleSlide = i
-        console.log(visibleSlide);
+document.getElementById("btnNext").addEventListener("click", function () {
+    // conteggio oggetti nell'array
+    for (let i = 0; i < Slider.length; i++) {
+        const img = Slider[i];
+        console.log(img);
+        // selezione oggetto con l'attributo che cerco e lo cambio
+        if (img.classList.contains('visible')) {
+            img.classList.remove('visible');
+            console.log(i, img);
+            // salvo l'elemento
+            visibleSlide = i
+            console.log(visibleSlide);
+        }
     }
-}
 
-// a fine cilco
-// incremento il numero dell'indice
-nextSlide = visibleSlide + 1;
-console.log(nextSlide);
+    // a fine cilco
+    // incremento il numero dell'indice
+    nextSlide = visibleSlide + 1;
+    console.log(nextSlide);
 
-// rinizio il ciclo
-if (nextSlide >= Slider.length) {
-    nextSlide = 0;
-}
+    // rinizio il ciclo
+    if (nextSlide >= Slider.length) {
+        nextSlide = 0;
+    }
 
-// aggiungo la classe visible alla nextSlide
-Slider[nextSlide].classList.add('visible');
+    // aggiungo la classe visible alla nextSlide
+    Slider[nextSlide].classList.add('visible');
 
-
+})
